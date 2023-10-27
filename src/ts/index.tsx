1292abd4ab12932bf9cf0f20ac68a2ad67401360
sylvain breton <sylvain.breton@gmail.com>
2023-10-27 16:12:51 -0400

    createRoot error gone

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App"; // Make sure the path to your App component is correct
 
 // Import your main SCSS file
 import '../scss/main.scss'; // Adjust the path to point to your main SCSS file
 
 const root = document.getElementById('root');
-if (root) {
-    const reactRoot = ReactDOM.createRoot(root);
-    reactRoot.render(<App />);
+if (!root) {
+    throw new Error('Root element not found');
 }
+
+const reactRoot = createRoot(root);
+reactRoot.render(<App />);
+
+
 
