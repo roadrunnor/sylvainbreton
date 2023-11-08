15ddcd6845089dd655370f31ffa20f2f9680bd1f
sylvain breton <sylvain.breton@gmail.com>
2023-11-08 16:37:37 -0500

    Header finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ const Header = () => {
 	return (
 		<header>
 			<div className="container">
+				
 				<div className="row">
 					{/* First column */}
 					<div className="col">
@@ -14,21 +15,18 @@ const Header = () => {
 							<span> ______________________________________ </span>
 						</blockquote>
 					</div>
-
 					{/* Second column */}
 					<div className="col">
 						<blockquote>
 							<span> ______________________________________ </span>
 						</blockquote>
 					</div>
-
 					{/* Third column */}
 					<div className="col">
 						<blockquote>
 							<span> ______________________________________ </span>
 						</blockquote>
 					</div>
-
 					{/* Fourth column */}
 					<div className="col">
 						<div>
@@ -37,9 +35,9 @@ const Header = () => {
 									<em>
 										"THE ONLY THING SELF-EVIDENT ABOUT THE CONTEMPORARY IS THAT
 										NOTHING CONCERNING THE CONTEMPORARY IS SEFT-EVIDENT ANYMORE"
-									</em>
+									</em>{" "}
 									- Joao Ribas, "What To Do With The Contemporary?",
-									<em>Ten Fundamental Questions of Curating</em>, Fiorucci Art
+									<em> Ten Fundamental Questions of Curating</em>, Fiorucci Art
 									Trust with Mousse Editions, Milan, Italy. 2011, p. 67. (From
 									Adorno Statement :
 									<em>
@@ -54,6 +52,15 @@ const Header = () => {
 						</div>
 					</div>
 				</div>
+
+				<div className="row">
+					<div className="col">
+						<blockquote>
+							<p>Background : Don Brown</p>
+						</blockquote>
+					</div>
+				</div>
+
 			</div>
 		</header>
 	);
