8fa53455e48be8eea933b69ef5bc741e1c941a85
sylvain breton <sylvain.breton@gmail.com>
2023-11-25 13:34:41 -0500

    Header.tsx et _header.scss corrigés dynamiquement

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,66 +1,51 @@
-// header.tsx
 import React from "react";
 import "../scss/_header.scss";
 
 const Header = () => {
 	return (
-		<header>
+		<header className="header">
 			<div className="container">
-				
 				<div className="row">
-					{/* First column */}
+					
 					<div className="col">
-						<blockquote>
-							<p>SYLVAIN BRETON</p>
-							<span> ______________________________________ </span>
-						</blockquote>
+						<p>SYLVAIN BRETON</p>
+						<div className="underline" />
 					</div>
-					{/* Second column */}
 					<div className="col">
-						<blockquote>
-							<span> ______________________________________ </span>
-						</blockquote>
+						<div className="underline" />
 					</div>
-					{/* Third column */}
 					<div className="col">
-						<blockquote>
-							<span> ______________________________________ </span>
-						</blockquote>
+						<div className="underline" />
 					</div>
-					{/* Fourth column */}
 					<div className="col">
-						<div>
-							<div className="head-marquee">
-								<div className="scrolling-text">
-									<em>
-										"THE ONLY THING SELF-EVIDENT ABOUT THE CONTEMPORARY IS THAT
-										NOTHING CONCERNING THE CONTEMPORARY IS SEFT-EVIDENT ANYMORE"
-									</em>{" "}
-									- Joao Ribas, "What To Do With The Contemporary?",
-									<em> Ten Fundamental Questions of Curating</em>, Fiorucci Art
-									Trust with Mousse Editions, Milan, Italy. 2011, p. 67. (From
-									Adorno Statement :
-									<em>
-										"IT IS SELF-EVIDENT THAT NOTHING CONCERNING ART IS
-										SEFL-EVIDENT ANYMORE, NOT ITS INNER LIFE, NOT ITS
-										RELATIONSHIP TO THE WORLD, NOT EVEN ITS RIGHT TO EXIST "
-									</em>
-									, Theodor Adorno, <em>Aesthetic Theory.</em>
-								</div>
+						<div className="head-marquee">
+							<div className="scrolling-text">
+								<em>
+									"THE ONLY THING SELF-EVIDENT ABOUT THE CONTEMPORARY IS THAT
+									NOTHING CONCERNING THE CONTEMPORARY IS SEFT-EVIDENT ANYMORE"
+								</em>{" "}
+								- Joao Ribas, "What To Do With The Contemporary?",
+								<em> Ten Fundamental Questions of Curating</em>, Fiorucci Art
+								Trust with Mousse Editions, Milan, Italy. 2011, p. 67. (From
+								Adorno Statement :
+								<em>
+									"IT IS SELF-EVIDENT THAT NOTHING CONCERNING ART IS
+									SEFL-EVIDENT ANYMORE, NOT ITS INNER LIFE, NOT ITS RELATIONSHIP
+									TO THE WORLD, NOT EVEN ITS RIGHT TO EXIST"
+								</em>
+								, Theodor Adorno, <em>Aesthetic Theory.</em>
 							</div>
-							<span>______________________________________</span>
 						</div>
+						<div className="underline" />
 					</div>
+					
 				</div>
 
 				<div className="row">
 					<div className="col">
-						<blockquote>
-							<p>Background : Don Brown</p>
-						</blockquote>
+						<p>Background : Don Brown</p>
 					</div>
 				</div>
-
 			</div>
 		</header>
 	);
