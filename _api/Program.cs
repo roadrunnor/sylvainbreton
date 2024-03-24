9ae5055b935877f9519b92b39ffc4f33a6973160
sylvain breton <sylvain.breton@gmail.com>
2024-03-24 16:35:43 -0400

    Fixed SSL

--- _api/Program.cs
+++ _api/Program.cs
@@ -1,10 +1,6 @@
 using api_sylvainbreton.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration.AddJsonFile("appsettings.json")
-                     .AddEnvironmentVariables();
-
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.WebHost.UseKestrelWithCertificate();
 
